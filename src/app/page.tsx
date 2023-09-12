cee8b384505d7177d666d71ef452678ff79b9042
khubaib-ak-47 <malikahma123d@gmail.com>
2023-09-12 16:38:05 +0500

    add smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,58 +8,30 @@ import {
   OurService,
 } from "./components";
 import React, { useEffect, useRef } from "react";
-import { motion, useAnimation } from "framer-motion";
+import LocomotiveScroll from "locomotive-scroll"; // locomotive-scroll package import karein
+import SmoothScroll from "./components/SmoothScroll";
 
 export default function Home() {
-  // const scrollRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef(null);
 
-  // useEffect(() => {
-  //   // Function to handle smooth scrolling
-  //   const handleScroll = (e: WheelEvent) => {
-  //     e.preventDefault();
-  //     const { deltaY } = e;
-  //     const currentScroll = scrollRef.current!.scrollTop;
-
-  //     // Adjust the scrolling speed by multiplying deltaY with a factor
-  //     const scrollSpeed = 8; // You can adjust this value to increase/decrease speed
-  //     const newScroll = currentScroll + deltaY * scrollSpeed;
-
-  //     // Perform smooth scrolling
-  //     scrollRef.current!.scrollTo({
-  //       top: newScroll,
-  //       behavior: "smooth",
-  //     });
-  //   };
-
-  //   const scrollRefCurrent = scrollRef.current; // Capture the current value
-
-  //   // Attach the scroll event listener to the scrollable container
-  //   scrollRefCurrent!.addEventListener("wheel", handleScroll, {
-  //     passive: false,
-  //   });
-
-  //   return () => {
-  //     // Remove the scroll event listener when the component unmounts
-  //     scrollRefCurrent!.removeEventListener("wheel", handleScroll);
-  //   };
-  // }, [scrollRef]); // Added scrollRef as a dependency
-
+  useEffect(() => {
+    if (scrollRef.current) {
+      const locomotive = new LocomotiveScroll({
+        el: scrollRef.current,
+        smooth: true, // Smooth scrolling enable karein
+        // Aur agar aur configurations chahiye to unhe yahan set karein
+      });
+    }
+  }, []);
   return (
-    <div
-    
-      // ref={scrollRef}
-      // style={{
-      //   height: "100vh",
-      //   overflowY: "scroll",
-      //   scrollBehavior: "smooth",
-      // }}
-      className="bg-black text-white w-full h-full  "
-    >
+    <div className="bg-black text-white w-full h-full  ">
       <Navigation />
-      <Hero />
-      <FeatureWork />
-      <OurService />
-      <JumpTo />
+      <SmoothScroll>
+        <Hero />
+        <FeatureWork />
+        <OurService />
+        <JumpTo />
+      </SmoothScroll>
 
       <Footer />
     </div>
